8463b80ed3d2e95a86d9640123b459e8203835a8
GHJung <futuremaker019@gmail.com>
2020-01-10 20:10:18 +0400

    transition-applied

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,34 @@
 <template>
   <div id="app">
     <tool-bar></tool-bar>
-    <router-view></router-view>
+    <transition name="page">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-import ToolBar from './components/ToolBar.vue'
+import ToolBar from "./components/ToolBar.vue";
 
 export default {
   components: {
-    ToolBar,
+    ToolBar
   }
 };
 </script>
 
 <style>
 body {
-  padding : 0;
-  margin: 0;  
+  padding: 0;
+  margin: 0;
+}
+
+/* Router Transition */
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+.page-enter, .page-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
