5b8bf87c8f928807130e1347ef589d62f975a361
GHJung <futuremaker019@gmail.com>
2020-01-11 20:41:47 +0400

    common-page-component-done

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,40 +1,13 @@
 <template>
   <div>
     <list-item></list-item>
-    <!-- <ul class="news-list">
-      <li v-for="item in fetchedAsk" class="post">
-        <div class="points">{{ item.points }}</div>
-        <div>
-          <router-link v-bind:to="`item/${item.id}`">
-            {{ item.title }}
-          </router-link>
-
-          <small class="link-text">
-            {{ item.time_ago }}
-            by
-            <router-link 
-            v-bind:to="`/user/${item.user}`" 
-            class="link-text">
-              {{ item.user }}
-            </router-link>
-          </small>
-        </div>
-      </li>
-    </ul> -->
   </div>
 </template>
 
 <script>
-// import { mapGetters } from "vuex";
 import ListItem from '../components/ListItem.vue'
 
 export default {
-  // computed: {
-  //   ...mapGetters(["fetchedAsk"])
-  // },
-  // created() {
-  //   this.$store.dispatch("FETCH_ASK");
-  // }
   components: {
     ListItem,
   }
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,24 +1,6 @@
 <template>
   <div>
     <list-item></list-item>
-    <!-- <ul class="news-list">
-      <li v-for="job in fetchedJobs" class="post">
-        <div class="points">
-          {{ job.points || 0}}
-        </div>
-        <div>
-          <p class="news-title">
-            <a :href="job.url">{{ job.title }}</a>
-          </p>
-          <small class="link-text">
-              {{ job.time_ago }} by
-            <a :href="job.url">
-              {{ job.domain }}
-            </a>        
-          </small>
-        </div>
-      </li>
-    </ul> -->
   </div>
 </template>
 
@@ -29,12 +11,6 @@ export default {
   components: {
     ListItem
   }
-  // computed: {
-  //   ...mapGetters(["fetchedJobs"])
-  // },
-  // created() {
-  //   this.$store.dispatch("FETCH_JOBS");
-  // }
 };
 </script>
 
