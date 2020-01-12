6dbb183c5047c59837107f710a50fb3cefaa4749
GHJung <futuremaker019@gmail.com>
2020-01-12 21:54:28 +0400

    slot-applied

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
-    <user-profile :info="userInfo"></user-profile>
+    <user-profile :info="userInfo">
+      <div slot="username">{{ userInfo.id }}</div>
+      <template slot="time">{{ userInfo.created }}</template>
+      <div slot="karma">{{ userInfo.karma }}</div>
+    </user-profile>
     <!-- <p>name : {{userInfo.id}}</p>
     <p>karma : {{userInfo.karma}}</p>
-    <p>created : {{userInfo.created}}</p> -->
+    <p>created : {{userInfo.created}}</p>-->
   </div>
 </template>
 
 <script>
-import UserProfile from '../components/UserProfile.vue'
+import UserProfile from "../components/UserProfile.vue";
 
 export default {
-  components:{
-    UserProfile,
+  components: {
+    UserProfile
   },
-  computed:{
+  computed: {
     userInfo() {
       return this.$store.state.user;
     }
@@ -22,11 +26,10 @@ export default {
   created() {
     console.log(this.$route);
     const userName = this.$route.params.id;
-    this.$store.dispatch('FETCH_USER', userName)
+    this.$store.dispatch("FETCH_USER", userName);
   }
-}
+};
 </script>
 
 <style>
-
 </style>
